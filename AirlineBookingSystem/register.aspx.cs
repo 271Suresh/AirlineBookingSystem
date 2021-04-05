7d595ea4f5bed07bda309017b7c08604a9a6e379
suresh suthar <sureshsuthar262626@outlook.com>
2021-04-05 22:22:13 +0530

    prevent duplicate email

--- AirlineBookingSystem/register.aspx.cs
+++ AirlineBookingSystem/register.aspx.cs
@@ -17,6 +17,7 @@ namespace AirlineBookingSystem
 
         protected void button_Click(object sender, EventArgs e)
         {
+           
             string fname = Request.Form.Get("fname");
             string lname = Request.Form.Get("lname");
             string gen = Request.Form.Get("gender");
@@ -28,12 +29,20 @@ namespace AirlineBookingSystem
             string pass = Request.Form.Get("pass");
             string cpass = Request.Form.Get("cpass");
 
-
-            //string mycon = "Server=localhost;Database=test1;Uid=root;Password= ;";
-            MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;");
-            MySqlCommand cmd = new MySqlCommand("insert into flyair.cust(fname,lname,gender,dob,city,state,email,phone,pass,cpass) values(@a1,@a2,@a3,@a4,@a5,@a6,@a7,@a8,@a9,@a10)", con);
-            try
+            MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;");
+            string query = "Select count(custid) from flyair.cust where email='"+ email+"'";
+            MySqlCommand cmdd = new MySqlCommand(query, conn);
+            int count = (int)cmdd.ExecuteScalar();
+            if (count > 0)
             {
+                Response.Write("<script>alert('Data Saved Successfully')</script>");
+            }
+            else
+            {
+                //string mycon = "Server=localhost;Database=test1;Uid=root;Password= ;";
+                MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;");
+                MySqlCommand cmd = new MySqlCommand("insert into flyair.cust(fname,lname,gender,dob,city,state,email,phone,pass,cpass) values(@a1,@a2,@a3,@a4,@a5,@a6,@a7,@a8,@a9,@a10)", con);
+
                 cmd.Parameters.AddWithValue("@a1", fname);
                 cmd.Parameters.AddWithValue("@a2", lname);
                 cmd.Parameters.AddWithValue("@a3", gen);
@@ -47,15 +56,9 @@ namespace AirlineBookingSystem
                 con.Open();
                 cmd.ExecuteNonQuery();
                 con.Close();
+                Response.Write("<script>alert('Data Saved Successfully')</script>");
+                Response.Redirect("login.aspx");
             }
-            catch (Exception ex)
-            {
-                Response.Write("<script>alert('Cannot use this email')</script>");
-                con.Close();
-                return;
-            }
-            Response.Write("<script>alert('Data Saved Successfully')</script>");
-            Response.Redirect("login.aspx");
 
         }
     }
