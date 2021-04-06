41abd362f95a9b274f0ca97590ccbb547c3a3ab6
pranathnaik <pranathnaik12@gmail.com>
2021-04-06 12:20:39 +0530

    email val

--- AirlineBookingSystem/register.aspx.cs
+++ AirlineBookingSystem/register.aspx.cs
@@ -36,34 +36,39 @@ namespace AirlineBookingSystem
             string query = "Select email from cust";
             MySqlCommand cmdd = new MySqlCommand(query, conn);
             MySqlDataReader mydr = cmdd.ExecuteReader();
+            int email_flag = 0;
             while (mydr.Read())
             {
-                if (email.Equals(mydr["email"].ToString()))
+              if (email.Equals(mydr["email"].ToString()))
                 {
-                    Response.Write("<script>alert('Email Already exists')</script>");
+                    email_flag = 1;
                 }
-                else
-                {
-                    //string mycon = "Server=localhost;Database=test1;Uid=root;Password= ;";
-                    MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;");
-                    MySqlCommand cmd = new MySqlCommand("insert into flyair.cust(fname,lname,gender,dob,city,state,email,phone,pass,cpass) values(@a1,@a2,@a3,@a4,@a5,@a6,@a7,@a8,@a9,@a10)", con);
+            }
 
-                    cmd.Parameters.AddWithValue("@a1", fname);
-                    cmd.Parameters.AddWithValue("@a2", lname);
-                    cmd.Parameters.AddWithValue("@a3", gen);
-                    cmd.Parameters.AddWithValue("@a4", dob);
-                    cmd.Parameters.AddWithValue("@a5", city);
-                    cmd.Parameters.AddWithValue("@a6", state);
-                    cmd.Parameters.AddWithValue("@a7", email);
-                    cmd.Parameters.AddWithValue("@a8", ph);
-                    cmd.Parameters.AddWithValue("@a9", pass);
-                    cmd.Parameters.AddWithValue("@a10", cpass);
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                    Response.Write("<script>alert('Data Saved Successfully')</script>");
-                    Response.Redirect("login.aspx");
-                }
+            if(email_flag == 1)
+            {
+                Response.Write("<script>alert('email already exist')</script>");
+            }
+            else
+            {  //string mycon = "Server=localhost;Database=test1;Uid=root;Password= ;";
+                MySqlConnection con = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;");
+                MySqlCommand cmd = new MySqlCommand("insert into flyair.cust(fname,lname,gender,dob,city,state,email,phone,pass,cpass) values(@a1,@a2,@a3,@a4,@a5,@a6,@a7,@a8,@a9,@a10)", con);
+
+                cmd.Parameters.AddWithValue("@a1", fname);
+                cmd.Parameters.AddWithValue("@a2", lname);
+                cmd.Parameters.AddWithValue("@a3", gen);
+                cmd.Parameters.AddWithValue("@a4", dob);
+                cmd.Parameters.AddWithValue("@a5", city);
+                cmd.Parameters.AddWithValue("@a6", state);
+                cmd.Parameters.AddWithValue("@a7", email);
+                cmd.Parameters.AddWithValue("@a8", ph);
+                cmd.Parameters.AddWithValue("@a9", pass);
+                cmd.Parameters.AddWithValue("@a10", cpass);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Data Saved Successfully')</script>");
+                Response.Redirect("login.aspx");
             }
 
         }
