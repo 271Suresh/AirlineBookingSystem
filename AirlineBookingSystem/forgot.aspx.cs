a9627c397128077283bba36bba09189c93f21ba6
suresh suthar <sureshsuthar262626@outlook.com>
2021-04-14 20:10:35 +0530

    forgot password

--- AirlineBookingSystem/forgot.aspx.cs
+++ AirlineBookingSystem/forgot.aspx.cs
@@ -24,40 +24,32 @@ namespace AirlineBookingSystem
 		protected void Button1_Click(object sender, EventArgs e)
 		{
             string email = Request.Form.Get("email");
-
             MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;database=flyair;username=root;password=;");
             conn.Open();
-            string query = "Select * from cust";
-            MySqlCommand cmdd = new MySqlCommand(query, conn);
+            MySqlCommand cmdd = new MySqlCommand("Select email,pass from cust where email='"+email+"'", conn);
             MySqlDataReader mydr = cmdd.ExecuteReader();
-            int email_flag = 0;
-            while (mydr.Read())
+            if (mydr.HasRows == true)
             {
-                if (email.Equals(mydr["email"].ToString()))
-                {
-                    email_flag = 1;
-                }
-                
-            }
-            string pass = mydr["pass"].ToString();
-            if (email_flag == 1)
-            {
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.Credentials = new System.Net.NetworkCredential("sutharsuresh271@gmail.com", "Suresh12345271");
-                smtp.EnableSsl = true;
-                MailMessage msg = new MailMessage();
-                msg.Subject = "Hello " + email;
-                msg.Body = "Your current password is "+pass;
-                string toaddress = email;
-                msg.To.Add(toaddress);
-                string fromaddress = "Suresh Suthar<sutharsuresh271@gmail.com>";
-                msg.From = new MailAddress(fromaddress);
-                smtp.Send(msg);
-                Response.Write("<script>alert('email sent')</script>");
-
-            }
+                mydr.Read();
+				//string eml = mydr["email"].ToString();
+				string pass = mydr["pass"].ToString();
+				//Response.Write(eml);
+				//Response.Write(pass);
+				SmtpClient smtp = new SmtpClient();
+				smtp.Host = "smtp.gmail.com";
+				smtp.Port = 587;
+				smtp.Credentials = new System.Net.NetworkCredential("flyairofficial00@gmail.com", "For@whileloop");
+				smtp.EnableSsl = true;
+				MailMessage msg = new MailMessage();
+				msg.Subject = "Hello " + email;
+				msg.Body = "Your current password is " + pass;
+				string toaddress = email;
+				msg.To.Add(toaddress);
+				string fromaddress = "FlyAir<flyairofficial00@gmail.com>";
+				msg.From = new MailAddress(fromaddress);
+				smtp.Send(msg);
+				Response.Write("<script>alert('email sent')</script>");
+			}
             else
             {
                 Response.Write("<script>alert('Invalid Username or Password')</script>");
